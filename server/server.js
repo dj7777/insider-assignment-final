a83e26beb6fc7e01230cd62bc001adf02c3d5d71
codewithdj <deepak_jain@persistent.com>
2017-07-28 17:56:22 +0530

    all tasks done
    Enhancement & code review left

--- server/server.js
+++ server/server.js
@@ -54,22 +54,123 @@ app.get("/", function(req, res) {
  });
 */
 
-app.post("/api/Upload755", function(req, res) {
-     upload(req, res, function(err) {
-         if (err) {
-             return res.end("Something went wrong!");
-         }
-
-         
-     })
-
+    function readFilesName(){
+        var albumId = 'Ex9l7';
+        var images =[];  
+        fs.readdir(publicPath+"/images", function(err, items) {
+        items.forEach(function(file){
+                imgur.uploadFile('public/images/'+file, albumId)
+                .then(function (json) {
+                    console.log(json.data.link);
+                })
+                .catch(function (err) {
+                    console.error(err.message);
+                })
+            });
+        });
+      };
+    
 
  app.post("/api/Upload", function(req, res) {
      upload(req, res, function(err) {
          if (err) {
              return res.end("Something went wrong!");
          }
-            
+         if(req.body.submit== '755*450')
+           { console.log(req.body);
+                 gm(req.files[0].path)
+            .resizeExact(755, 450)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_1" + "_" + req.files[0].originalname, function (err) {
+                if (!err){
+                    console.log('done'); 
+                    readFilesName();   
+                    } 
+            else console.log(err);
+        });
+             
+           }
+        else if(req.body.submit== '365*450')
+           { console.log(req.body);
+                             gm('images/'+req.files[0].filename)
+            .resizeExact(365, 450)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_2" + "_" + req.files[0].originalname, function (err) {
+            if (!err){
+                console.log('done');
+                readFilesName();
+                } 
+           else console.log(err);
+        });
+             return res.redirect('/images');
+           }
+
+            else if(req.body.submit== '365*212')
+           { console.log(req.body);
+                gm('images/'+req.files[0].filename)
+            .resizeExact(365, 212)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_3"+ "_" + req.files[0].originalname, function (err) {
+            if (!err) {
+                console.log('done');
+                 readFilesName();
+            }
+            });
+            return res.redirect('/images');
+           }
+                else if(req.body.submit== '380*380')
+           { console.log(req.body);
+
+               gm('images/'+req.files[0].filename)
+            .resizeExact(380, 380)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_4" + "_" +req.files[0].originalname, function (err) {
+            if (!err){
+                 readFilesName();
+             console.log('done');
+            }
+        });
+         return res.redirect('/images');
+   }
+
+
+         else if(req.body.submit== 'Upload')
+           { 
+               console.log(req.body);
+
+                gm(req.files[0].path)
+            .resizeExact(755, 450)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_1" + "_" + req.files[0].originalname, function (err) {
+                if (!err){
+                    console.log('done');    
+                    } 
+            else console.log(err);
+            });
+
+
+            gm('images/'+req.files[0].filename)
+            .resizeExact(365, 450)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_2" + "_" + req.files[0].originalname, function (err) {
+            if (!err){
+                console.log('done');
+                } 
+           else console.log(err);
+        });
+ 
+
+            gm('images/'+req.files[0].filename)
+            .resizeExact(365, 212)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_3"+ "_" + req.files[0].originalname, function (err) {
+            if (!err) console.log('done');
+            });
+
+            gm('images/'+req.files[0].filename)
+            .resizeExact(380, 380)
+            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_4" + "_" +req.files[0].originalname, function (err) {
+            if (!err){
+                 readFilesName();
+             console.log('done');
+            }
+        });
+         return res.redirect('/images');
+        }
+
 /*
           var resizefile1=  gm(req.files[0].path)
             .resizeExact(755, 450);
@@ -105,59 +206,7 @@ app.post("/api/Upload755", function(req, res) {
             });
 */
 
-            var resizefile1=  gm(req.files[0].path)
-            .resizeExact(755, 450)
-            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_1" + "_" + req.files[0].originalname, function (err) {
-            if (!err){
-                console.log('done');    
-                } 
-           else console.log(err);
-        });
-
-
-            gm('images/'+req.files[0].filename)
-            .resizeExact(365, 450)
-            .write('public/images/'+req.files[0].fieldname + "_" + Date.now()+"_2" + "_" + req.files[0].originalname, function (err) {
-            if (!err){
-                console.log('done');
-                } 
-           else console.log(err);
-        });
- 
-
-            gm('images/'+req.files[0].filename)
-            .resizeExact(365, 212)
-            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_3"+ "_" + req.files[0].originalname, function (err) {
-            if (!err) console.log('done');
-            });
-
-            gm('images/'+req.files[0].filename)
-            .resizeExact(380, 380)
-            .write('public/images/'+req.files[0].fieldname + "_" + Date.now() +"_4" + "_" +req.files[0].originalname, function (err) {
-            if (!err){
-                 readFilesName();
-             console.log('done');
-            }
-            });
-
-function readFilesName(){
-     var images =[];  
-   fs.readdir(publicPath+"/images", function(err, items) {
-   
-//console.log(items);
-
- items.forEach(function(file){
-            imgur.uploadFile('public/images/'+file)
-            .then(function (json) {
-                console.log(json.data.link);
-            })
-            .catch(function (err) {
-                console.error(err.message);
-            })
-     });
- });
- }
-            return res.redirect('/images');
+           return res.redirect('/images');
      });
  });
 
@@ -168,36 +217,21 @@ function readFilesName(){
      console.log("Listening to port 2000");
  });
 
- app.get('/images', function(req, res, next) {
-
-//var jsonRes='';
- var kittenAlbum = 'wnst8';
-    imgur.getAlbumInfo(kittenAlbum)
-    .then(function(json) {
-      //  console.log(json);
-       var imageLists = '';
-            for (var i=0; i<json.data.images_count; i++) {
-                imageLists += '<img height="200px" width="200px" src="' + json.data.images[i].link +  '"/>';        
-            }
-            imageLists += '';
-            res.writeHead(200, {'Content-type':'text/html'});
-            res.end(imageLists);
-
-      //  console.log(json.data.images[1].link);
-    //    jsonRes= json;
-    
-/*res.setHeader('Access-Control-Allow-Origin', '/images');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.send(JSON.stringify({ data: json }));  
-*/  //res.sendFile(publicPath + '/images.html', JSON.stringify({data: req.body}));
-
-    })
-    .catch(function (err) {
-        console.error(err.message);
+    app.get('/images', function(req, res, next) {
+        var InsiderAlbum = 'Ex9l7';
+        imgur.getAlbumInfo(InsiderAlbum)
+        .then(function(json) {
+        //  console.log(json);
+        var imageLists = '';
+                for (var i=0; i<json.data.images_count; i++) {
+                    imageLists += '<img height="200px" width="200px" src="' + json.data.images[i].link +  '"/>';        
+                }
+                imageLists += '';
+                res.writeHead(200, {'Content-type':'text/html'});
+                res.end(imageLists);
+        })
+        .catch(function (err) {
+            console.error(err.message);
+        });
     });
 
-
-
-    // console.log(jsonRes);
-  //res.json({ data: jsonRes });
-});
